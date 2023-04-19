fb80d7c2a02802b09033f93b68310657d14acd3d
Bohdana_Vashchuk <bogdana.vashchuk@gmail.com>
2023-04-19 10:48:10 +0300

    commit test

--- src/main/java/org/calculator/SomeClass.java
+++ src/main/java/org/calculator/SomeClass.java
@@ -4,6 +4,7 @@ public class SomeClass {
     public int valueforfield;
     public int valueforfield1;
     public int valueforfield2;
+    public int test;
 
     public static void testMethod(){
     }
